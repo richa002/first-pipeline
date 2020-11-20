7f2a1d4aa53e771612cfa661113f2532fccce261
Richa Luthra <richaluthra.ca@gmail.com>
2020-11-20 15:36:42 -0500

    changes made for testing webhook on jenkins-ec2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world.........");
         System.out.println("changes made");
         System.out.println("Vivek cahnges");
+        System.out.println("richa made changes for jenkins -ec2");
     }
 }
